e69c41da83530413aa0a8cd10c19c543b8d4e895
Hakan Acundas <hakan.acundas@gmail.com>
2022-08-29 15:56:33 +0300

    Calculation of model, view, projection matrices for camera tutorial.

--- OpenGL/Application.cpp
+++ OpenGL/Application.cpp
@@ -5,6 +5,9 @@
 #include "res/shaders/ShadersManeger.h"
 #include "res/vendor/stb_image/stb_image.h"
 #include "API/TextureManager.h"
+#include "res/vendor/glm/glm.hpp"
+#include "res/vendor/glm/gtc/matrix_transform.hpp"
+#include "res/vendor/glm/gtc/type_ptr.hpp"
 
 #define WIDTH 860
 #define HEIGHT 640
@@ -76,6 +79,50 @@ int main(int argc, char* argv[])
 		-0.5f,  0.5f ,0.0f,		1.0f, 1.0f, 0.0f,	0.0f, 1.0f  // top left
 	};
 
+	float vertices36[] = {
+	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+	 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+	};
+
 	float texCoords[] = {
 	0.0f, 0.0f,  // lower-left corner  
 	1.0f, 0.0f,  // lower-right corner
@@ -89,6 +136,7 @@ int main(int argc, char* argv[])
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 	glewInit();
+	glEnable(GL_DEPTH_TEST);
 
 	VertexArray vertexArrayObject;
 	VertexBuffer vertexBufferObject(sizeof(vertices), vertices);
@@ -98,11 +146,11 @@ int main(int argc, char* argv[])
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementBuffer);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(6 * sizeof(float)));
 	glEnableVertexAttribArray(2);
 
 	std::string filePath = "res/shaders/shaders.shader";
@@ -114,17 +162,32 @@ int main(int argc, char* argv[])
 	std::string imagePath = "res/textures/ATAM.jpg";
 	//int width, height, nrChannels;
 	texture.TextureFlipVertically();
-	texture.LoadTexture(imagePath, 500, 500, 3);
+	texture.LoadTexture(imagePath, 1000, 1000, 3);
 	texture.FreeImageData();
-
+	
 	glUseProgram(shaderProgram);
+
+	int modelLoc = glGetUniformLocation(shaderProgram, "model");
+	int viewLoc = glGetUniformLocation(shaderProgram, "view");
+	int projectionLoc = glGetUniformLocation(shaderProgram, "projection");
 
 	while (!glfwWindowShouldClose(window))
 	{
 		handleInput(window);
 		//resizeWindow();
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		
+		glm::mat4 model = glm::mat4(1.0f);
+		glm::mat4 view = glm::mat4(1.0f);
+		glm::mat4 projection;
+		model = glm::rotate(model, glm::radians((GLfloat)glfwGetTime() * 50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+		view = glm::translate(view, glm::vec3(-0.2f, -0.2f, -3.0f));
+		projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
--- OpenGL/res/shaders/shaders.shader
+++ OpenGL/res/shaders/shaders.shader
@@ -5,12 +5,15 @@ layout(location = 0) in vec3 inputPosition;
 layout(location = 1) in vec3 inputColor;
 layout(location = 2) in vec2 inputTextureCoordinates;
 
-out vec3 vertexColor;
 out vec2 vertexTextureCoordinates;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
 void main()
 {
-	gl_Position = vec4(inputPosition, 1.0);
-	vertexColor = inputColor;
+	gl_Position = projection * view * model * vec4(inputPosition, 1.0);
 	vertexTextureCoordinates = inputTextureCoordinates;
 }
 
@@ -18,7 +21,6 @@ void main()
 #version 330 core
 
 out vec4 fragmentColor;
-in vec3 vertexColor;
 in vec2 vertexTextureCoordinates;
 
 uniform sampler2D fragmentTexture;
