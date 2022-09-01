19dac117a4747e45f274a6266e199cf8665f6574
Hakan Acundas <hakan.acundas@gmail.com>
2022-09-01 16:11:14 +0300

    Camera Class Header

--- OpenGL/API/Camera.h
+++ OpenGL/API/Camera.h
@@ -2,7 +2,6 @@
 
 #ifndef CAMERA_H
 #define CAMERA_H
-#endif
 
 #include <gl/glew.h>
 #include "../res/vendor/glm/glm.hpp"
@@ -23,3 +22,32 @@ const float PITCH = 0.0f;
 const float SPEED = 2.5f;
 const float SENSITIVITY = 0.1f;
 const float ZOOM = 45.0f;
+
+class Camera
+{
+public:
+    glm::vec3 Position;
+    glm::vec3 Front;
+    glm::vec3 Up;
+    glm::vec3 Right;
+    glm::vec3 WorldUp;
+    // euler Angles
+    float Yaw;
+    float Pitch;
+    // camera options
+    float MovementSpeed;
+    float MouseSensitivity;
+    float Zoom;
+
+	Camera();
+	~Camera();
+    glm::mat4 GetViewMatrix();
+    void ProcessKeyboard(CameraMovement direction, float deltaTime);
+    void ProcessMouseMovement(float xoffset, float yoffset, GLboolean constrainPitch = true);
+    void ProcessMouseScroll(float yoffset);
+
+private:
+    void updateCameraVectors();
+};
+
+#endif
